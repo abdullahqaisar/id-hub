2f1f7bbf37a6b1b6abc0ce177b888c2aaf1e8584
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-08-27 12:55:40 +0500

    add config and axios functionality

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 import { Response } from 'express';
+import { firstValueFrom } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getReqParams(query: any, res: Response): void {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getReqParams(query: any, res: Response): Promise<void> {
     console.log('Query Params:', query);
-    // Redirect to a specific URL
-    res.redirect('https://login-iaajtj-dev2.fa.ocs.oraclecloud.com/');
+
+    const clientId = this.configService.get<string>('CLIENT_ID');
+    const clientSecret = this.configService.get<string>('CLIENT_SECRET');
+
+    const data = {
+      client_id: clientId,
+      client_secret: clientSecret,
+      grant_type: 'authorization_code',
+      response_type: 'code',
+      redirect_uri: 'https://id-hub.vercel.app/callback-url',
+      code: query.code,
+    };
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'cache-control': 'no-cache',
+      },
+    };
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(
+          'https://id-hub.vercel.app/callback-url?abd=xyz&param=a',
+          data,
+          config,
+        ),
+      );
+
+      console.log('Response:', response);
+
+      // Redirect to a specific URL
+      res.redirect('https://login-iaajtj-dev2.fa.ocs.oraclecloud.com/');
+    } catch (error) {
+      throw error;
+    }
   }
 }
